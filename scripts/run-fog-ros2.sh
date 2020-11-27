34967d99b14f9ea81bab94c3817be7daae722544
Jari Nippula <jarix@ssrc.tii.ae>
2020-11-27 17:07:12 +0200

    Added missing parameter flag for communication_link start

--- scripts/run-fog-ros2.sh
+++ scripts/run-fog-ros2.sh
@@ -9,4 +9,4 @@ ros2 launch px4_mavlink_ctrl mavlink_ctrl.launch >${logs_dir}/mav_ctrl_out.log 2
 echo "Start Micrortps_agent"
 micrortps_agent -t UDP >${logs_dir}/urtps_out.log 2>${logs_dir}/urtps_err.log &
 echo "Start Communication link"
-communication_link -device_id "${drone_device_id}" "/enclave/rsa_private.pem" >${logs_dir}/commlink_out.log 2>${logs_dir}/commlink_err.log
+communication_link -device_id "${drone_device_id}" -private_key "/enclave/rsa_private.pem" >${logs_dir}/commlink_err.log 2>${logs_dir}/commlink_out.log
