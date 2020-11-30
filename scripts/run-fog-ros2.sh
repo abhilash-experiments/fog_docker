ea59044158a02e759fd67a467082a0afda44ec9f
Jari Nippula <jarix@ssrc.tii.ae>
2020-11-30 13:18:17 +0200

    Add missing env export for ROS2 namespace definition

--- scripts/run-fog-ros2.sh
+++ scripts/run-fog-ros2.sh
@@ -2,6 +2,8 @@ source /opt/ros/foxy/setup.bash
 drone_device_id=${1}
 logs_dir=${2}
 
+export DRONE_DEVICE_ID=${drone_device_id}
+
 echo "Start Mavlink Router"
 mavlink-routerd >${logs_dir}/mav_routerd_out.log 2>${logs_dir}/mav_routerd_err.log &
 echo "Start Mavlink control"
