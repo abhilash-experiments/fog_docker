a164a3d4d3b81673f29c3fa384ccf3ea8906b555
Jari Nippula <jarix@ssrc.tii.ae>
2020-11-30 13:19:29 +0200

    Temporarily remove communication-link node
    Node removed until dependency issues are fixed

--- scripts/run-fog-ros2.sh
+++ scripts/run-fog-ros2.sh
@@ -9,6 +9,4 @@ mavlink-routerd >${logs_dir}/mav_routerd_out.log 2>${logs_dir}/mav_routerd_err.l
 echo "Start Mavlink control"
 ros2 launch px4_mavlink_ctrl mavlink_ctrl.launch >${logs_dir}/mav_ctrl_out.log 2>${logs_dir}/mav_ctrl_err.log &
 echo "Start Micrortps_agent"
-micrortps_agent -t UDP >${logs_dir}/urtps_out.log 2>${logs_dir}/urtps_err.log &
-echo "Start Communication link"
-communication_link -device_id "${drone_device_id}" "/enclave/rsa_private.pem" >${logs_dir}/commlink_out.log 2>${logs_dir}/commlink_err.log
+micrortps_agent -t UDP >${logs_dir}/urtps_out.log 2>${logs_dir}/urtps_err.log
