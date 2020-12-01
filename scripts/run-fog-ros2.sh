40c830aaed5c72ca33fbd83218c0c241a8490e7a
Jari Nippula <jarix@ssrc.tii.ae>
2020-12-01 16:17:33 +0200

    Use namespace in micrortps_agent

--- scripts/run-fog-ros2.sh
+++ scripts/run-fog-ros2.sh
@@ -9,4 +9,4 @@ mavlink-routerd >${logs_dir}/mav_routerd_out.log 2>${logs_dir}/mav_routerd_err.l
 echo "Start Mavlink control"
 ros2 launch px4_mavlink_ctrl mavlink_ctrl.launch >${logs_dir}/mav_ctrl_out.log 2>${logs_dir}/mav_ctrl_err.log &
 echo "Start Micrortps_agent"
-micrortps_agent -t UDP >${logs_dir}/urtps_out.log 2>${logs_dir}/urtps_err.log
+micrortps_agent -t UDP -n ${drone_device_id} >${logs_dir}/urtps_out.log 2>${logs_dir}/urtps_err.log
